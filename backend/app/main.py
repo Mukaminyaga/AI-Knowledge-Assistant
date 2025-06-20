b883f6a328f0dc558f7402d2e6a9a1dfc36e69f3
Maureen Nyaga <mukamiganya@gmail.com>
2025-06-20 10:50:20 +0300

    Changed how the api calls the backend in Login,Signup and upload document

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:8000"],
+      allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "http://192.168.1.249:3000" 
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
